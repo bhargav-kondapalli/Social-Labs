9a4f243308acff73bec4316786d25461b883c9a5
Bhargav Kondapalli <bhargavkondapalli@Bhargavs-MacBook-Pro.local>
2016-09-19 22:25:12 -0700

    Completed setting up Firebase Integration

---
+++ Social-Labs/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social-Labs
+//
+//  Created by Bhargav Kondapalli on 9/19/16.
+//  Copyright © 2016 KikrLabs. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
