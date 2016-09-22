727ef7d849b868e40bacc7d5f4bc73a249ea895f
Bhargav Kondapalli <bhargavkondapalli@Bhargavs-MacBook-Pro.local>
2016-09-22 15:58:20 -0700

    Sign in UI custom classes complete

---
+++ Social-Labs/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social-Labs
+//
+//  Created by Bhargav Kondapalli on 9/22/16.
+//  Copyright © 2016 KikrLabs. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Social-Labs/FancyField.swift
@@ -0,0 +1,30 @@
+//
+//  FancyField.swift
+//  Social-Labs
+//
+//  Created by Bhargav Kondapalli on 9/22/16.
+//  Copyright © 2016 KikrLabs. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
